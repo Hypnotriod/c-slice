1208f258349ac5989a7bbaf8d3a100b34f417b15
illia <illia.pikin@gmail.com>
2025-01-28 17:51:47 +0200

    print more info

--- main.c
+++ main.c
@@ -435,13 +435,13 @@ void slc_extract_all(slice_t* slice, void* buff) {
 // ******* Usage example *******
 // *****************************
 
-void print_slice_info(const slice_t* slice) {
-    printf("len=%i, cap=%i, size=%i, final=%s: ",
-            slice->l, slice->c, slice->s, slice->is_final ? "true" : "false");
+void print_slice_info(const char* info, const slice_t* slice) {
+    printf("%s:\r\n  len=%i, cap=%i, size=%i, final=%s: ",
+            info, slice->l, slice->c, slice->s, slice->is_final ? "true" : "false");
 }
 
-void print_slice_ints(const slice_t* slice) {
-    print_slice_info(slice);
+void print_slice_ints(const char* info, const slice_t* slice) {
+    print_slice_info(info, slice);
     for (int i = 0; i < slice->l;) {
         printf("%i, ", slci(slice, int, i++));
     }
@@ -452,62 +452,65 @@ int main() {
     int ints[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     const slice_t slice_ints = slc_from_arr(ints);
-    print_slice_ints(&slice_ints);
+    print_slice_ints("Static slice_t from array", &slice_ints);
 
     const slice_t slice_of_arr = slc_slice_arr(ints, 3, 5);
-    print_slice_ints(&slice_of_arr);
+    print_slice_ints("Static slice_t from array slice from 3 with length 5", &slice_of_arr);
 
     slice_t* slice_ints_new_from = slc_new_from_arr(ints, 10);
-    print_slice_ints(slice_ints_new_from);
+    print_slice_ints("New slice_t* from array slice with extra cap 10", slice_ints_new_from);
+
+    slc_shrink(slice_ints_new_from, 2);
+    print_slice_ints("slice_t* after shrink by 2", slice_ints_new_from);
 
     slice_ints_new_from = slc_append_arr(slice_ints_new_from, ints, __arr_size(ints));
-    print_slice_ints(slice_ints_new_from);
+    print_slice_ints("slice_t* after array append", slice_ints_new_from);
 
     slc_free(slice_ints_new_from);
     slice_ints_new_from = slc_new_from_slice(&slice_ints, 10);
-    print_slice_ints(slice_ints_new_from);
+    print_slice_ints("New slice_t* from slice of slice_t* with extra cap 10", slice_ints_new_from);
 
     slice_t* slice_ints_new = slc_new(sizeof (int), slice_ints.l, 2);
-    print_slice_ints(slice_ints_new);
+    print_slice_ints("New empty slice_t* with length 9 and extra cap 2", slice_ints_new);
     for (int i = 0; i < slice_ints_new->l; i++) {
         // ((int*) slice_ints_new->p)[i] = ((int*) slice_ints.p)[i];
-        slci(slice_ints_new, int, i) = slci(&slice_ints, int, i);
+        slci(slice_ints_new, int, i) = -slci(&slice_ints, int, i);
     }
-    print_slice_ints(slice_ints_new);
+    print_slice_ints("slice_t* after data copy with sign inversion", slice_ints_new);
 
     slice_ints_new = slc_append_slice(slice_ints_new, &slice_ints);
-    print_slice_ints(slice_ints_new);
+    print_slice_ints("slice_t* after slice append", slice_ints_new);
 
     slc_append(slice_ints_new, int, 55);
-    print_slice_ints(slice_ints_new);
+    print_slice_ints("slice_t* after append 55", slice_ints_new);
 
     const slice_t slice_of_slice1 = slc_slice(slice_ints_new, 5, -1);
-    print_slice_ints(&slice_of_slice1);
+    print_slice_ints("Static slice_t from slice_t* from 5 to the full length", &slice_of_slice1);
 
     const slice_t slice_of_slice2 = slc_slice(slice_ints_new, -8, -1);
-    print_slice_ints(&slice_of_slice2);
+    print_slice_ints("Static slice_t from slice_t* from -8 to the full length", &slice_of_slice2);
 
     slc_free(slice_ints_new_from);
     slice_ints_new_from = slc_new(sizeof (int), 0, 12);
     slice_ints_new_from = slc_append_slice_n(slice_ints_new_from, 2, &slice_of_slice1, &slice_of_slice2);
-    print_slice_ints(slice_ints_new_from);
+    print_slice_ints("slice_t* after several appended slices", slice_ints_new_from);
 
     slc_free(slice_ints_new_from);
     slice_ints_new_from = slc_slice_new(slice_ints_new, 3, 5);
-    print_slice_ints(slice_ints_new_from);
+    print_slice_ints("New slice_t* from slice_t* starting from 3 and length 5", slice_ints_new_from);
 
     slice_t* slice_concatenated = slc_concat(&slice_of_slice1, &slice_of_slice2);
     slc_append(slice_concatenated, int, 44);
     slc_append(slice_concatenated, int, 22);
     slc_append(slice_concatenated, int, 11);
-    print_slice_ints(slice_concatenated);
+    print_slice_ints("New slice_t* after two slices concatenated and 44, 22, 11 values added", slice_concatenated);
 
     slc_free(slice_concatenated);
     slice_concatenated = slc_concat_n(3, &slice_of_slice1, slice_ints_new_from, &slice_of_slice2);
     slc_append(slice_concatenated, int, 33);
     slc_append(slice_concatenated, int, 66);
     slc_append(slice_concatenated, int, 77);
-    print_slice_ints(slice_concatenated);
+    print_slice_ints("New slice_t* after three slices concatenated and 33, 66, 77 values added", slice_concatenated);
 
     slc_free(slice_ints_new_from);
     slc_free(slice_ints_new);
